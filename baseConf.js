e9f18f671ad83d0b61cbd79ba00f789beb7263bb
super181818 <131360198+super181818@users.noreply.github.com>
2023-11-28 00:11:52 +0800

    Add files via upload

---
+++ baseConf.js
@@ -0,0 +1,31 @@
+
+	var apiUrl = 'http://kbb.panguh5.com:2201/hello.php';
+	var websiteUrl,appid,baseCDN,version = '';
+	var queryUrl = apiUrl
+	var urlParams = parseURLParams(window.location.href)
+	if (urlParams.room){
+	    queryUrl = queryUrl + '?room=' + urlParams.room;
+	} else if (urlParams.cardNo){
+	    $("#cardno").html(urlParams.cardNo);
+	}else if (urlParams.skinNo){
+	    $("#skinno").html(urlParams.skinNo);
+	}else if (urlParams.headNo){
+	    $("#headno").html(urlParams.headNo);
+	}else if(urlParams.invitation) {
+	     $("#invitation").html(urlParams.invitation);
+	}else{
+	    queryUrl = queryUrl
+	}
+     
+	
+	//getCurrentAgent();
+       
+
+
+    
+
+
+	
+    
+    
+    
